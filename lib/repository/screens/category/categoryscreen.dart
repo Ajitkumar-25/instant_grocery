b2e60db63b7905e663a86868904c11ba1d872f74
Ajitkumar-25 <kajit0408@gmail.com>
2025-01-10 12:37:57 +0530

    started with category section

--- lib/repository/screens/category/categoryscreen.dart
+++ lib/repository/screens/category/categoryscreen.dart
@@ -1,16 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:blinkit_clone/repository/widgets/uihelper.dart';
 
 class CategoryScreen extends StatelessWidget {
-  const CategoryScreen({super.key});
+  TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Category Screen',
-          style: TextStyle(fontSize: 30, color: Colors.blue),
-        ),
+      body: Column(
+        children: [
+          SizedBox(height: 36),
+          Stack(
+            children: [
+              Container(
+                height: 180,
+                width: double.infinity,
+                color: Color(0XFFF7CB45),
+                child: Column(
+                  children: [
+                    SizedBox(height: 40),
+                    Row(
+                      children: [
+                        SizedBox(width: 20),
+                        UiHelper.CustomText(
+                            text: 'Blinkit in',
+                            color: Color(0XFF000000),
+                            fontweight: FontWeight.bold,
+                            fontsize: 14,
+                            fontfamily: 'bold'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(width: 20),
+                        UiHelper.CustomText(
+                            text: '16 minutes',
+                            color: Color(0XFF000000),
+                            fontweight: FontWeight.bold,
+                            fontsize: 20,
+                            fontfamily: 'bold')
+                      ],
+                    ),
+                    Row(children: [
+                      SizedBox(width: 20),
+                      UiHelper.CustomText(
+                          text: 'HOME',
+                          color: Color(0XFF000000),
+                          fontweight: FontWeight.bold,
+                          fontsize: 14),
+                      UiHelper.CustomText(
+                          text: '- Sujal Dave, Ratanada, Jodhpur (Raj)',
+                          color: Color(0XFF000000),
+                          fontweight: FontWeight.w300,
+                          fontsize: 14)
+                    ]),
+                  ],
+                ),
+              ),
+              Positioned(
+                right: 20,
+                bottom: 100,
+                child: CircleAvatar(
+                  radius: 15,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.black,
+                    size: 20,
+                  ),
+                ),
+              ),
+              Positioned(
+                  bottom: 16,
+                  left: 15,
+                  child: UiHelper.CustomTextField(controller: searchController))
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SizedBox(width: 20),
+              UiHelper.CustomText(
+                  text: 'Grocery & Kitchen',
+                  color: Color(0XFF000000),
+                  fontweight: FontWeight.bold,
+                  fontsize: 14,
+                  fontfamily: 'bold')
+            ],
+          )
+        ],
       ),
     );
   }
